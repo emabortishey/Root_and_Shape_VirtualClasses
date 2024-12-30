fe27868243962133ac631362ada414f28533cfd1
fartb <fartbreather228@gmail.com>
2024-12-31 00:40:49 +0300

    создан класс линейного уравнения и проведён тест на вычисление его корня

--- Root.h
+++ Root.h
@@ -9,3 +9,19 @@ class Root
 public:
 	virtual T get_root() = 0;
 };
+
+template<class T>
+class Line_root : public Root<T>
+{
+	T x;
+	T value;
+
+public:
+	Line_root(T x_P, T val_P) : x{ x_P }, value{ val_P } { };
+
+	virtual T get_root() override final
+	{
+		T result; 
+		return (x && value) ? -value / x : 0;
+	}
+};
--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,9 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
+	Line_root<int> meow(6, 36);
 
+	cout << meow.get_root();
 
 	return 0;
 }
